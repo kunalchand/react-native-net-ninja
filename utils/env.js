f08f59c3211b7d3fbc67910a8fb9ef49e305147d
Kunal Chand <kunalchand234@gmail.com>
2025-09-30 22:46:40 -0400

    fix env

--- utils/env.js
+++ utils/env.js
@@ -1,17 +1,4 @@
-// utils/env.js
-
-function getEnvVar(name, fallback) {
-  const value = process.env[name];
-
-  if (value === undefined) {
-    if (fallback !== undefined) return fallback;
-    throw new Error(`Missing required environment variable: ${name}`);
-  }
-
-  return value;
-}
-
 export const getEnv = () => ({
-  APPWRITE_PROJECT_ID: getEnvVar("APPWRITE_PROJECT_ID"),
-  APPWRITE_PLATFORM: getEnvVar("APPWRITE_PLATFORM"),
+  APPWRITE_PROJECT_ID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  APPWRITE_PLATFORM: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
 });
